e69c47b0d11debfb919227d25900c9f163bbf707
lucasgardes <75265271+lucasgardes@users.noreply.github.com>
2026-07-24 09:57:26 +0200

    feat(collection): expose collection IPC bridge

--- preload.js
+++ preload.js
@@ -6,3 +6,8 @@ contextBridge.exposeInMainWorld("chv1Media", {
   scanLibrary: () => ipcRenderer.invoke("media:scan-library"),
   chooseLibrary: () => ipcRenderer.invoke("media:choose-library")
 });
+
+contextBridge.exposeInMainWorld("chv1Collection", {
+  get: () => ipcRenderer.invoke("collection:get"),
+  update: (payload) => ipcRenderer.invoke("collection:update", payload)
+});
