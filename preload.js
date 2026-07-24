6607a1e858dae810d6e116e0d4361a13ef3f2549
lucasgardes <75265271+lucasgardes@users.noreply.github.com>
2026-07-24 10:08:47 +0200

    Expose media import APIs

--- preload.js
+++ preload.js
@@ -4,7 +4,11 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("chv1Media", {
   scanLibrary: () => ipcRenderer.invoke("media:scan-library"),
-  chooseLibrary: () => ipcRenderer.invoke("media:choose-library")
+  chooseLibrary: () => ipcRenderer.invoke("media:choose-library"),
+  getLibraryPath: () => ipcRenderer.invoke("media:get-library-path"),
+  chooseImportFiles: (kind) => ipcRenderer.invoke("media:choose-import-files", kind),
+  importVideo: (payload) => ipcRenderer.invoke("media:import-video", payload),
+  importImage: (payload) => ipcRenderer.invoke("media:import-image", payload)
 });
 
 contextBridge.exposeInMainWorld("chv1Collection", {
